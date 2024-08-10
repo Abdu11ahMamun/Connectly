76e87f831491f44cd5f286d988c129de7b2c027a
abdu11ah-mamun <cs.abdullah.mamun@gmail.com>
2024-08-10 23:40:18 +0600

    sidebar half done

--- src/main/java/com/java/Connectly/Contoller/UserController.java
+++ src/main/java/com/java/Connectly/Contoller/UserController.java
@@ -15,10 +15,54 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.security.Principal;
+
 @Controller
-@RequestMapping("user")
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
 
-
-
+    @RequestMapping("/index")
+    public String dashboard(Model model, Principal principal){
+        String userName=principal.getName(); //get the username from session: in this case email
+        User user= userRepository.getUserByEmail(userName);
+        model.addAttribute("user", user);
+        return "userPages/dashboard";
+    }
 }
+
+/*
+* +-----------------------+     +-----------------------+
+|  User Login Form       | --> |  AuthenticationManager |
++-----------------------+     +-----------------------+
+                                    |
+                                    v
+                         +-----------------------+
+                         | DaoAuthenticationProvider |
+                         +-----------------------+
+                                    |
+                                    v
+                         +-----------------------+
+                         |  UserDetailsService    |
+                         +-----------------------+
+                                    |
+                                    v
+                         +-----------------------+
+                         |    UserDetails (Principal)  |
+                         +-----------------------+
+                                    |
+                                    v
+                        +-------------------------+
+                        |    SecurityContext      |
+                        +-------------------------+
+                                    |
+                                    v
+                         +-----------------------+
+                         |     Controller          |
+                         |  (Using Principal)      |
+                         +-----------------------+
+
+*
+*
+* */
